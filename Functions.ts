e0dd6db58f18be49104cd46857879936125059da
luis.tejaxun <luistejaxunrz@gmail.com>
2021-01-25 00:07:18 -0600

    Tipo de función 'void, string, number, Array<number>' parámetros opcionales

--- Functions.ts
+++ Functions.ts
@@ -1,5 +1,31 @@
+let user = {
+    name: "luis",
+    password: "ABCDE",
+    role: ["admin"]
+}
 
-// Función con parámetros opcionales
+// Sin retorno de valor
+function imprimirNombreUsuario() : void {
+    console.log(user.name);
+}
+
+// Retorna la suma de x, y (number)
+function suma(x : number, y : number) : number {
+    return x + y;
+}
+
+// Retorna un string con formato.
+let miSuma = function(x : number, y : number) : string {
+    return `la suma de ${x} + ${y} es igual a ${x + y}`;
+}
+
+// Retorna un Array de los números x, y
+let miArreglo = function(x: number, y: number) : Array<number> {
+    return [x, y];
+}
+
+
+// Parámetros opcionales
 let nombreCompleto = function(
     primerNombre: string,
     apellido: string,
