3986fd704ad6919974066ca7d9af26af2d0ea96f
luis.tejaxun <luistejaxunrz@gmail.com>
2021-01-24 22:23:12 -0600

    Expresión condicional 'if'

---
+++ controlFlujo.ts
@@ -0,0 +1,42 @@
+// If (Expresión condicional)
+
+/*
+if (condición) {
+    declaración 1;
+} else {
+    declaración 2;
+}
+*/
+
+/*
+if (condición_1) {
+    declaración 1;
+} else if (condición_2) {
+    declaración 2;
+} else {
+    declaración 3;
+}
+*/
+
+let edad : number = 17;
+let mayoriaEdad : number = 18;
+
+if (mayoriaEdad >= 18) {
+    console.log('Eres mayor de edad.');
+} else {
+    console.log('Eres menor de edad.');
+}
+
+
+// ***************************************
+let numero : number = 2;
+
+if (numero == 2) {
+    
+} else if (numero == 3) {
+
+} else if (numero == 4) {
+
+} else {
+
+}
