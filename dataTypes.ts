6d1a84b4262375ecbcba3ad7214c49891ff64868
luis.tejaxun <luistejaxunrz@gmail.com>
2021-01-24 19:43:46 -0600

    Tipo de dato 'number'

--- dataTypes.ts
+++ dataTypes.ts
@@ -1,3 +1,6 @@
-// Booleano
-let bool : boolean = false;
-
+// Numérico
+let int: number = 8;
+let decimal: number = 6.65;
+let hex: number = 0xf00d;
+let binario: number = 0b1010;
+let octal: number = 0o744;
