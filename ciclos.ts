32553d0fa6adc69ccc463716db67c4348961f524
luis.tejaxun <luistejaxunrz@gmail.com>
2021-01-25 00:49:54 -0600

    Ciclos en TS (JS)

--- ciclos.ts
+++ ciclos.ts
@@ -5,6 +5,31 @@ let nombres : Array<string> = [
     "Luis"
 ];
 
+// Ciclo For
+for (let i = 0; i < nombres.length; i++) {
+    const name = nombres[i];
+    console.log(name);
+}
+
+
+// Ciclo While
+let j : number = 0;
+
+while (j < nombres.length) {
+    console.log(nombres[j]);
+    j++;
+}
+
+
+// Ciclo do - while
+let i : number = 0;
+
+do {
+    console.log(nombres[i]);
+    i++;
+} while (i < nombres.length);
+
+
 // Método forEach
 nombres.forEach((item : string) => {
     console.log(item);
