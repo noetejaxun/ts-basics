7d29fb6489d204ac0c156691ffee685b3debcb53
luis.tejaxun <luistejaxunrz@gmail.com>
2021-01-25 00:35:54 -0600

    Ciclo For

---
+++ ciclos.ts
@@ -0,0 +1,12 @@
+let nombres : Array<string> = [
+    "Hazel",
+    "Stuart",
+    "Daniel",
+    "Luis"
+];
+
+// Ciclo For
+for (let i = 0; i < nombres.length; i++) {
+    const name = nombres[i];
+    console.log(name);
+}
