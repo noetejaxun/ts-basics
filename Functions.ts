eb598d4bc614e96d5c3124e99639e229e66755e8
luis.tejaxun <luistejaxunrz@gmail.com>
2021-01-25 00:04:21 -0600

    Tipo de función 'number, string, Array<number>'

--- Functions.ts
+++ Functions.ts
@@ -1,12 +1,18 @@
-let user = {
-    name: "luis",
-    password: "ABCDE",
-    role: ["admin"]
+// Funciones con retorno de valor
+
+// Retorna la suma de x, y (number)
+function suma(x : number, y : number) : number {
+    return x + y;
 }
 
-// Sin retorno de valor
-function imprimirNombreUsuario() : void {
-    console.log(user.name);
+
+// Retorna un string con formato.
+let miSuma = function(x : number, y : number) : string {
+    return `la suma de ${x} + ${y} es igual a ${x + y}`;
 }
 
-imprimirNombreUsuario();
+
+// Retorna un Array de los números x, y
+let miArreglo = function(x: number, y: number) : Array<number> {
+    return [x, y];
+}
