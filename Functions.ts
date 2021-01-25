a5698c05fa4594fb90e69b428a4f8c6845d79576
luis.tejaxun <luistejaxunrz@gmail.com>
2021-01-25 00:05:12 -0600

    Tipo de función 'string' parámetros opcionales

--- Functions.ts
+++ Functions.ts
@@ -1,18 +1,13 @@
-// Funciones con retorno de valor
 
-// Retorna la suma de x, y (number)
-function suma(x : number, y : number) : number {
-    return x + y;
-}
-
-
-// Retorna un string con formato.
-let miSuma = function(x : number, y : number) : string {
-    return `la suma de ${x} + ${y} es igual a ${x + y}`;
-}
-
-
-// Retorna un Array de los números x, y
-let miArreglo = function(x: number, y: number) : Array<number> {
-    return [x, y];
+// Función con parámetros opcionales
+let nombreCompleto = function(
+    primerNombre: string,
+    apellido: string,
+    apellidoCasada?: string) : string {
+    
+    if (apellidoCasada) {
+        return `${primerNombre} ${apellido} de ${apellidoCasada}`;
+    } else {
+        return `${primerNombre} ${apellido}`;
+    }
 }
